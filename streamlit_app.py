db1e1f466b79528c241759b8f76d8e680efc1aa4
mashhar <120915865+mashhar@users.noreply.github.com>
2025-05-10 15:08:18 +0530

    Update streamlit_app.py
    added API import statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+#new section to import smoothiefroot nutrition information
+import requests
+smoothiefroot_response=request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothies :cup_with_straw:")
 st.write(
